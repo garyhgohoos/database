fb4cb4f6ec75d96b1f3d2027f78a5c8f5d4a23d1
garyhgohoos <gary@xtuple.com>
2014-04-01 12:10:16 -0400

    Issue #23119:disallow multiple unposted cobmisc for same S/O

--- dbscripts/functions/deleteinvoice.sql
+++ dbscripts/functions/deleteinvoice.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE FUNCTION deleteInvoice(INTEGER) RETURNS INTEGER AS $$
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   pInvcheadid ALIAS FOR $1;
+  _cobmiscid INTEGER := -1;
 
 BEGIN
   UPDATE shipitem SET shipitem_invoiced=FALSE, shipitem_invcitem_id=NULL
@@ -18,19 +19,38 @@ BEGIN
 			WHERE ((cobill_invcitem_id=invcitem_id)
 			  AND  (invcitem_invchead_id=pInvcheadid)))));
 
-  UPDATE cobill SET cobill_invcnum=NULL, cobill_invcitem_id=NULL
-  FROM invcitem
-  WHERE ((cobill_invcitem_id=invcitem_id)
-    AND  (invcitem_invchead_id=pInvcheadid));
-
   UPDATE invdetail SET invdetail_invcitem_id=NULL
   FROM invcitem
   WHERE ((invdetail_invcitem_id=invcitem_id)
     AND  (invcitem_invchead_id=pInvcheadid));
 
-  UPDATE cobmisc SET cobmisc_invcnumber=NULL, cobmisc_invchead_id=NULL,
+  -- Check for unposted cobmisc for the same S/O
+  -- If found then consolidate
+  SELECT cobmisc_id INTO _cobmiscid
+  FROM cobmisc
+  WHERE ((NOT cobmisc_posted)
+    AND  (cobmisc_cohead_id IN (SELECT coitem_cohead_id
+                                FROM invcitem JOIN coitem ON (coitem_id=invcitem_coitem_id)
+                                WHERE (invcitem_invchead_id=pInvcheadid))))
+  LIMIT 1;
+  IF (FOUND) THEN
+    UPDATE cobill SET cobill_invcnum=NULL, cobill_invcitem_id=NULL, cobill_cobmisc_id=_cobmiscid
+    FROM invcitem
+    WHERE ((cobill_invcitem_id=invcitem_id)
+      AND  (invcitem_invchead_id=pInvcheadid));
+
+    DELETE FROM cobmisc
+    WHERE (cobmisc_invchead_id=pInvcheadid);
+  ELSE
+    UPDATE cobill SET cobill_invcnum=NULL, cobill_invcitem_id=NULL
+    FROM invcitem
+    WHERE ((cobill_invcitem_id=invcitem_id)
+      AND  (invcitem_invchead_id=pInvcheadid));
+
+    UPDATE cobmisc SET cobmisc_invcnumber=NULL, cobmisc_invchead_id=NULL,
 		     cobmisc_posted=FALSE
-  WHERE (cobmisc_invchead_id=pInvcheadid);
+    WHERE (cobmisc_invchead_id=pInvcheadid);
+  END IF;
 
   DELETE FROM aropenalloc
   WHERE (aropenalloc_doctype='I')
